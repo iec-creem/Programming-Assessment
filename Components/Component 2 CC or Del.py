bc7624635d33b1afa29b6df6c263eb04338134db
Ean Siao <20058@my.sanctamaria.school.nz>
2025-03-27 14:56:37 +1300

    version 3 of cc or del component

--- Components/Component 2 CC or Del.py
+++ Components/Component 2 CC or Del.py
@@ -1,14 +1,21 @@
+LOW = 1
+HIGH = 2
 print("Do you want click and collect or delivery?")
 print("Enter 1 for click and collect")
 print("Enter 2 for delivery")
 
-try:
-    choice = int(input("please enter 1 or 2"))
-except ValueError:
-    print("That is not a valid number")
+while True:
+    try:
+        num = int(input("please enter 1 or 2"))
+        if num >= LOW and num <= HIGH:
+            if num == 1:
+                print("Click and collect")
+                break
+            elif num == 2:
+                print("Delivery")
+                break
+            else: num = int(input("Please enter 1 or 2"))
+    except ValueError:
+        print("That is not a valid number please only enter 1 or 2")
 
-if choice == 1:
-    print("Click and collect")
-elif choice == 2:
-    print("Delivery")
-else: print("Error")
+print("continue")
