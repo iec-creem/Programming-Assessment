b2ac2bdb3fd713795b3c35b3ec542b2ad8ea8eb3
Ean Siao <20058@my.sanctamaria.school.nz>
2025-03-27 14:40:06 +1300

    Version 1 of cc or del component

---
+++ Components/Component 2 CC or Del.py
@@ -0,0 +1,10 @@
+print("Do you want click and collect or delivery?")
+print("Enter 1 for click and collect")
+print("Enter 2 for delivery")
+choice = int(input("please enter 1 or 2"))
+if choice == 1:
+    print("Click and collect")
+elif choice == 2:
+    print("Delivery")
+else:
+    print("error")
