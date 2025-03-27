011730e214d6d95aaeee02304e1aca9db8a9466e
Ean Siao <20058@my.sanctamaria.school.nz>
2025-03-27 14:45:16 +1300

    version 2 of cc or del component

--- Components/Component 2 CC or Del.py
+++ Components/Component 2 CC or Del.py
@@ -1,10 +1,14 @@
 print("Do you want click and collect or delivery?")
 print("Enter 1 for click and collect")
 print("Enter 2 for delivery")
-choice = int(input("please enter 1 or 2"))
+
+try:
+    choice = int(input("please enter 1 or 2"))
+except ValueError:
+    print("That is not a valid number")
+
 if choice == 1:
     print("Click and collect")
 elif choice == 2:
     print("Delivery")
-else:
-    print("error")
+else: print("Error")
