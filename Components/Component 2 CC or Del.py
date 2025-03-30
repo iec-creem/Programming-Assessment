8813d196fc03cd0524147c421a2d60e1c86b8c64
Ean Siao <20058@my.sanctamaria.school.nz>
2025-03-31 12:43:41 +1300

    version 4 of cc or del component

--- Components/Component 2 CC or Del.py
+++ Components/Component 2 CC or Del.py
@@ -1,21 +1,37 @@
+# Constant variables for low and high number for menus 
 LOW = 1
 HIGH = 2
-print("Do you want click and collect or delivery?")
-print("Enter 1 for click and collect")
-print("Enter 2 for delivery")
 
-while True:
-    try:
-        num = int(input("please enter 1 or 2"))
-        if num >= LOW and num <= HIGH:
-            if num == 1:
-                print("Click and collect")
-                break
-            elif num == 2:
-                print("Delivery")
-                break
-            else: num = int(input("Please enter 1 or 2"))
-    except ValueError:
-        print("That is not a valid number please only enter 1 or 2")
+# Function validates integers
+# Takes parameters of low and high numbers and question
+# Input must be integer between low and high parameters 
+# While loop until correct input is received then returns input to original function
+# Value error results in error message and new input request 
+def integer_validation(low, high, question): 
+    while True:
+        try:
+            num = int(input(question))
+            if num >= low and num <= high:
+                return num
+            else:
+                (f"please enter {LOW} or {HIGH}: ")
+        except ValueError:
+            print("That is not a valid number")
+            (f"please enter {LOW} or {HIGH}: ")
 
-print("continue")
+# Function allowing users to choose either click and collect or delivery
+# Input request sent to integer validation function for validation
+# Valid input is returned and sent to if statements for appropriate action
+def pickup_delivery():
+    del_pick = ""
+    print("Do you want click and collect or delivery?")
+    question = (f"please enter {LOW} or {HIGH}: ")
+    print("Enter 1 for click and collect")
+    print("Enter 2 for delivery")
+    del_pick = integer_validation(LOW, HIGH, question)
+    if del_pick == 1:
+        print("Click and collect")
+    elif del_pick == 2:
+        print("Delivery")
+
+pickup_delivery()
