2f29200d60a3cfa7ec23de9484ceb97bc8447c2e
Ean Siao <20058@my.sanctamaria.school.nz>
2025-03-31 12:56:17 +1300

    version 1 of customer details component

---
+++ Components/Component 3 part 1 customer details.py
@@ -0,0 +1,16 @@
+# Setting up variables
+name = ""
+phone = ""
+
+# Ask for user input
+print("Please enter your name: ")
+name = input()
+print("Please enter your phone number: ")
+phone = input()
+print("Please enter your address: ")
+address = input()
+
+# Printing customer details
+print(name)
+print(phone)
+print(address)
