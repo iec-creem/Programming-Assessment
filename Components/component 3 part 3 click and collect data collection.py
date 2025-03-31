887fe6e02afc3dac720e77ac06315d1e0458e706
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-01 10:10:45 +1300

    version 2 of CC data collection component

--- Components/component 3 part 3 click and collect data collection.py
+++ Components/component 3 part 3 click and collect data collection.py
@@ -6,30 +6,33 @@ customer_details = {}
 # Regular expression pattern for phone validation
 pattern = r"^\d{8,10}$"
 
-# While loop for validation of name
-while True:
-    question = "Please enter your name: "
-    response = input(question)
-    # Removes blank spaces from response
-    no_blanks = re.sub(r"\s+", "", response)
-    # Checking if input is alphabetical
-    x = no_blanks.isalpha()
-    if x == False:
-        # If not then print error message
-        print("Input must only contain letters")
-    else:
-        # If alpha covert to title and append to dictionary
-        customer_details["name"] = response.title()
-        break
+def click_collect():
+    # While loop for validation of name
+    while True:
+        question = "Please enter your name: "
+        response = input(question)
+        # Removes blank spaces from response
+        no_blanks = re.sub(r"\s+", "", response)
+        # Checking if input is alphabetical
+        x = no_blanks.isalpha()
+        if x == False:
+            # If not then print error message
+            print("Input must only contain letters")
+        else:
+            # If alpha covert to title and append to dictionary
+            customer_details["name"] = response.title()
+            break
 
-# While loop for validation of phone number
-while True:
-    # Asks for user input
-    phone_number = input("Please enter your phone number: ")
-    if re.match(pattern, phone_number):
-        customer_details["phone"] = phone_number
-        break
-    else:
-        print("This is an invalid phone number")
+    # While loop for validation of phone number
+    while True:
+        # Asks for user input
+        phone_number = input("Please enter your phone number: ")
+        if re.match(pattern, phone_number):
+            customer_details["phone"] = phone_number
+            break
+        else:
+            print("This is an invalid phone number")
+
+click_collect()
 
 print(customer_details)
