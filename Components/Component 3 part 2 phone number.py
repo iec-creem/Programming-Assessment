b6841d8929ef1e48d4f3b5fb276f7b4343793ee8
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-01 09:42:10 +1300

    version 2 of phone number component

--- Components/Component 3 part 2 phone number.py
+++ Components/Component 3 part 2 phone number.py
@@ -1,8 +1,16 @@
+import re
+
 # Customer details dictionary
 customer_details = {}
 
-# Ask for user input
-customer_details["phone"] = input("Please enter your phone number: ")
+# Regular expression pattern for phone validation
+pattern = r"^\d{8,10}$"
 
+# Ask for user input
+phone_number = input("Please enter your phone number: ")
+if re.match(pattern, phone_number):
+    customer_details["phone"] = phone_number
+else:
+    print("this is an invalid phone number")
 
 print(customer_details)
