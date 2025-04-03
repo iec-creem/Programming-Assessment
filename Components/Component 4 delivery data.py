d0265180f2c887d3da0429e28659ad1db794e946
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-04 12:18:12 +1300

    version 4 of delivery data component

--- Components/Component 4 delivery data.py
+++ Components/Component 4 delivery data.py
@@ -20,6 +20,18 @@ def validate_alpha(question):
 
 
 def delivery_info():
+    # While loop for validation of house number
+    while True:
+        question = "Please enter your house or apartment number: "
+        response = input(question)
+        if response == "":
+            print ("Can not be left blank")
+        else:
+            # Removes blank spaces from response
+            no_blanks = re.sub(r"\s+", "", response)
+            customer_details["house"] = response.title()
+            break
+
     question = "Please enter your street name: "
     response = validate_alpha(question)
     # If alpha covert to title and append to dictionary
