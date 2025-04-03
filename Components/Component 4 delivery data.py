b7956deaa0d32c1ce53d365d6500e75deb939e43
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-04 12:09:50 +1300

    version 2 of delivery data component

--- Components/Component 4 delivery data.py
+++ Components/Component 4 delivery data.py
@@ -3,17 +3,36 @@ import re
 # Customer details dictionary
 customer_details = {}
 
-# While loop for validation of house number
+# While loop for validation of street name
 while True:
-    question = "Please enter your house or apartment number: "
+    question = "Please enter your street name: "
     response = input(question)
-    if response == "":
-        print ("Can not be left blank")
+    # Removes blank spaces from response
+    no_blanks = re.sub(r"\s+", "", response)
+    # Checking if input is alphabetical
+    x = no_blanks.isalpha()
+    if x == False:
+        # If not then print error message
+        print("Input must only contain letters")
     else:
-        # Removes blank spaces from response
-        no_blanks = re.sub(r"\s+", "", response)
-        customer_details["house"] = response.title()
+        # If alpha covert to title and append to dictionary
+        customer_details["street"] = response.title()
         break
 
+# While loop for validation of suburb
+while True:
+    question = "Please enter your suburb name: "
+    response = input(question)
+    # Removes blank spaces from response
+    no_blanks = re.sub(r"\s+", "", response)
+    # Checking if input is alphabetical
+    x = no_blanks.isalpha()
+    if x == False:
+        # If not then print error message
+        print("Input must only contain letters")
+    else:
+        # If alpha covert to title and append to dictionary
+        customer_details["suburb"] = response.title()
+        break
 
 print(customer_details)
