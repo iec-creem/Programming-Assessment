f077d22a72dd364e54081a8e6c7058f2418c09ab
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-07 09:42:16 +1200

    version 1 of menu component

---
+++ Components/Component 5 menu.py
@@ -0,0 +1,10 @@
+boba_names = ['Original Milk Tea', 'Strawberry Milk Tea', 'Chocolate Milk Tea', 'Coffee Milk Tea', 'Mocha Milk Tea','Taro Milk Tea', 'Caramel Milk Tea', 'Matcha Milk Tea','Brown Sugar Milk Tea',  
+               'Black Tea', 'Jasmine Green Tea', 'Peach Tea', 'Passion Fruit Tea', 'Mango Tea', 
+               'Grape Slushy', 'Lemon Slushy', 'Lime Slushy', 'Strawberry Slushy', 'Mango Slushy', 'Peach Slushy', 'Passion Fruit Slushy', 'Kiwifruit Slushy', 'Green Apple Slushy', 'Pineapple Slushy']
+
+boba_prices = [8.00, 8.00, 8.00, 8.00, 8.00, 8.20, 8.20, 8.60, 10.50, 7.20, 7.20, 7.90, 7.90, 7.90, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20]
+
+NUMBER_BOBA = 24
+for count in range(NUMBER_BOBA):
+    print("{} {} {}".format(count, boba_names[count], boba_prices[count]))
+    # weeeeeeee
