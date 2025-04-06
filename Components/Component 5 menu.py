076cb024101e09210d607404030c262a95ce4443
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-07 09:59:50 +1200

    version 3 of menu component

--- Components/Component 5 menu.py
+++ Components/Component 5 menu.py
@@ -7,7 +7,9 @@ boba_names = ['Original Milk Tea', 'Strawberry Milk Tea', 'Chocolate Milk Tea', 
 boba_prices = [8.00, 8.00, 8.00, 8.00, 8.00, 8.20, 8.20, 8.60, 10.50, 7.20, 7.20, 7.90, 7.90, 7.90, 
                9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20]
 
-NUMBER_BOBA = 24
-for count in range(NUMBER_BOBA):
-    print("{} {} ${:.2f}".format(count+1, boba_names[count], boba_prices[count]))
-    
+def boba_menu():
+    NUMBER_BOBA = 24
+    for count in range(NUMBER_BOBA):
+        print("{} {} ${:.2f}".format(count+1, boba_names[count], boba_prices[count]))
+
+boba_menu()
