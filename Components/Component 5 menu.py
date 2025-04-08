30fcaa3c8d1a92a31d6e89c12cdac9f10dda3675
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-09 11:46:03 +1200

    version 5 of menu component

--- Components/Component 5 menu.py
+++ Components/Component 5 menu.py
@@ -4,9 +4,11 @@ import pandas as pd
 # Create menu dictionary
 menu_dict = {}
 
+# Format pizza prices as currency
+pd.options.display.float_format = '${:,.2f}'.format
+
 # Add pizza numbers to dictionary
 menu_dict ['Number'] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
-
 
 menu_dict ['Boba Names'] = ['Original Milk Tea', 'Strawberry Milk Tea', 'Chocolate Milk Tea', 'Coffee Milk Tea', 'Mocha Milk Tea','Taro Milk Tea', 
               'Caramel Milk Tea', 'Matcha Milk Tea','Brown Sugar Milk Tea',  
