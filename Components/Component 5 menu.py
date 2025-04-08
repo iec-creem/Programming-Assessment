1cbbfe75683d6432300a994d29fb902c75ca7e51
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-09 11:41:57 +1200

    version 4 of menu component

--- Components/Component 5 menu.py
+++ Components/Component 5 menu.py
@@ -1,15 +1,24 @@
-boba_names = ['Original Milk Tea', 'Strawberry Milk Tea', 'Chocolate Milk Tea', 'Coffee Milk Tea', 'Mocha Milk Tea','Taro Milk Tea', 
+# Import pandas library 
+import pandas as pd
+
+# Create menu dictionary
+menu_dict = {}
+
+# Add pizza numbers to dictionary
+menu_dict ['Number'] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+
+
+menu_dict ['Boba Names'] = ['Original Milk Tea', 'Strawberry Milk Tea', 'Chocolate Milk Tea', 'Coffee Milk Tea', 'Mocha Milk Tea','Taro Milk Tea', 
               'Caramel Milk Tea', 'Matcha Milk Tea','Brown Sugar Milk Tea',  
                'Black Tea', 'Jasmine Green Tea', 'Peach Tea', 'Passion Fruit Tea', 'Mango Tea', 
                'Grape Slushy', 'Lemon Slushy', 'Lime Slushy', 'Strawberry Slushy', 'Mango Slushy', 'Peach Slushy', 'Passion Fruit Slushy', 
                'Kiwifruit Slushy', 'Green Apple Slushy', 'Pineapple Slushy']
 
-boba_prices = [8.00, 8.00, 8.00, 8.00, 8.00, 8.20, 8.20, 8.60, 10.50, 7.20, 7.20, 7.90, 7.90, 7.90, 
-               9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20]
+menu_dict ['Boba Prices'] = [8.00, 8.00, 8.00, 8.00, 8.00, 8.20, 8.20, 8.60, 10.50, 7.20, 7.20, 7.90, 7.90, 7.90, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20, 9.20]
 
-def boba_menu():
-    NUMBER_BOBA = 24
-    for count in range(NUMBER_BOBA):
-        print("{} {} ${:.2f}".format(count+1, boba_names[count], boba_prices[count]))
+# Display menu dataframe
+df =pd.DataFrame(menu_dict)
+blankIndex = [''] * len(df)
+df.index = blankIndex
 
-boba_menu()
+print("Pizza Menu \n\n" ,df)
