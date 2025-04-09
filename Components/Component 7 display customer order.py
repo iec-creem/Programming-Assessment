07a6e7a3e6e08f303814aa699a2d6d4c7bb6e5ff
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-09 13:16:23 +1200

    version 2 of display order component

--- Components/Component 7 display customer order.py
+++ Components/Component 7 display customer order.py
@@ -4,6 +4,11 @@ order_list = ['Original Milk Tea', 'Black Tea', 'Grape Slushy', 'Pineapple Slush
 # List to store pizza prices
 order_cost = [8.00, 7.20, 9.20, 9.20]
 
+# Customer details
+customer_details = {'name': 'Rhamz Dela Torre', 'phone': '021021021', 'house': '17Gabe', 'street': 'Rhamz', 'suburb': 'Aaron'}
+
+print(customer_details)
+
 count = 0
 for item in order_list:
     print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
