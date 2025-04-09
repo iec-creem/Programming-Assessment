3b153b678a88c1d7ee083a8b89ce2246d1555756
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-09 13:24:13 +1200

    version 4 of display order component

--- Components/Component 7 display customer order.py
+++ Components/Component 7 display customer order.py
@@ -1,3 +1,5 @@
+from colorama import Fore, Back, Style, init
+
 # List to store ordered pizzas
 order_list = ['Original Milk Tea', 'Black Tea', 'Grape Slushy', 'Pineapple Slushy']
 
@@ -6,10 +8,16 @@ order_cost = [8.00, 7.20, 9.20, 9.20]
 
 # Customer details
 customer_details = {'name': 'Rhamz Dela Torre', 'phone': '021021021', 'house': '17Gabe', 'street': 'Rhamz', 'suburb': 'Aaron'}
+init(autoreset=True)
 
+print()
+# Print customer order
+print(Fore.GREEN + "Customer Details")
 print(f"Customer Name: {customer_details['name']}\nCustomer Phone: {customer_details['phone']}\nCustomer Address: {customer_details['house']} {customer_details['street']} {customer_details['suburb']}")
-
+print()
+print(Fore.GREEN + "Order Details")
 count = 0
 for item in order_list:
-    print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
+    print(Style.BRIGHT + "Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
     count = count+1
+print()
