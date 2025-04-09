7ea7c13dc1550673b2eea8e784cc95cb273dd4e7
Ean Siao <20058@my.sanctamaria.school.nz>
2025-04-09 13:27:58 +1200

    version 5 of display order component

--- Components/Component 7 display customer order.py
+++ Components/Component 7 display customer order.py
@@ -10,14 +10,17 @@ order_cost = [8.00, 7.20, 9.20, 9.20]
 customer_details = {'name': 'Rhamz Dela Torre', 'phone': '021021021', 'house': '17Gabe', 'street': 'Rhamz', 'suburb': 'Aaron'}
 init(autoreset=True)
 
-print()
-# Print customer order
-print(Fore.GREEN + "Customer Details")
-print(f"Customer Name: {customer_details['name']}\nCustomer Phone: {customer_details['phone']}\nCustomer Address: {customer_details['house']} {customer_details['street']} {customer_details['suburb']}")
-print()
-print(Fore.GREEN + "Order Details")
-count = 0
-for item in order_list:
-    print(Style.BRIGHT + "Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
-    count = count+1
-print()
+def print_order():
+    print()
+    # Print customer order
+    print(Fore.GREEN + "Customer Details")
+    print(f"Customer Name: {customer_details['name']}\nCustomer Phone: {customer_details['phone']}\nCustomer Address: {customer_details['house']} {customer_details['street']} {customer_details['suburb']}")
+    print()
+    print(Fore.GREEN + "Order Details")
+    count = 0
+    for item in order_list:
+        print(Style.BRIGHT + "Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
+        count = count+1
+    print()
+
+print_order()
